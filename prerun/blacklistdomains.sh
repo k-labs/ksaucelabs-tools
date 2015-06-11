8c1bbb105f5c2d48846010927696baa72d59173d
Gaetano Giunta <ggiunta@kaliop.com>
2015-06-11 17:06:40 +0200

    one more test

--- prerun/blacklistdomains.sh
+++ prerun/blacklistdomains.sh
@@ -5,7 +5,7 @@ echo > /tmp/hosts
 
 for var in "$@"
 do
-    echo "localhost $var" >> /tmp/hostaliases
+    echo "127.0.0.1 $var" >> /tmp/hostaliases
 done
 cat /etc/hosts >> /tmp/hostaliases
 cp -y /tmp/hostaliases /etc/hosts
@@ -16,4 +16,4 @@ sudo cp -y /tmp/hostaliases /etc/hosts
 
 # if running via sauceconnect - see https://support.saucelabs.com/customer/portal/articles/2005376-edit-the-domain-name-system-dns-within-the-sauce-labs-virtual-machine-vm-
 #
-export HOSTALIASES=/tmp/hostaliases
+#export HOSTALIASES=/tmp/hostaliases

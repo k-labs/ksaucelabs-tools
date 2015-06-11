58c247a561e382e5c4455a5433508291d77728b5
Gaetano Giunta <ggiunta@kaliop.com>
2015-06-11 16:04:58 +0200

    export env var

--- prerun/blacklistdomains.bat
+++ prerun/blacklistdomains.bat
@@ -1,6 +1,7 @@
 @echo off
 REM loop over all input arguments (a list of domain names) and blacklist them via pointing them to localhost
 REM idea from https://github.com/albedithdiaz/sauceSupport/blob/master/EditDNS_Windows.bat
+REM has to be run from an elevated prompt
 
 echo # > %temp%\temphosts.txt
 
@@ -19,3 +20,8 @@ type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 REM type %temp%\temphosts.txt
 
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
+
+REM in case copying fails - see https://support.saucelabs.com/customer/portal/articles/2005376-edit-the-domain-name-system-dns-within-the-sauce-labs-virtual-machine-vm-
+
+set HOSTALIASES=/tmp/hostaliases
+setx HOSTALIASES=/tmp/hostaliases
--- prerun/blacklistdomains.sh
+++ prerun/blacklistdomains.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # loop over all input arguments (a list of domain names) and blacklist them via pointing them to localhost
 
+echo > /tmp/hosts
+
 for var in "$@"
 do
-    echo "127.0.0.1 $var" >> /etc/hosts
+    echo "127.0.0.1 $var" >> /tmp/hosts
 done
+cat /etc/hosts >> /tmp/hosts
+cp -y /tmp/hosts /etc/hosts
+
+# in case copying fails - see https://support.saucelabs.com/customer/portal/articles/2005376-edit-the-domain-name-system-dns-within-the-sauce-labs-virtual-machine-vm-
+
+export HOSTALIASES=/tmp/hostaliases

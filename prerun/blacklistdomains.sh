66e58e5fd4e493cc2daf8e8c7e4f2319ededad7c
Gaetano Giunta <ggiunta@kaliop.com>
2015-06-11 18:54:34 +0200

    final version?

--- prerun/blacklistdomains.sh
+++ prerun/blacklistdomains.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 # loop over all input arguments (a list of domain names) and blacklist them via pointing them to localhost
 
-echo > /tmp/hostaliases
+# retrieve public IP of a VM owned by kaliop which we never use to host sites
+# this can take a couple of seconds, but we try not to blacklist any IP which MIGHT get used...
+# (using localhost IPs does not seem to work)
+BLACKHOLEIP=`dig +short ec2-54-148-20-76.us-west-2.compute.amazonaws.com`
 
+echo > /tmp/hostaliases
 for var in "$@"
 do
-    echo "127.127.127.127 $var" >> /tmp/hostaliases
+    echo "$BLACKHOLEIP $var" >> /tmp/hostaliases
 done
 cat /etc/hosts >> /tmp/hostaliases
+
 cp -f /tmp/hostaliases /etc/hosts 2>/dev/null
 if [ $? -ne 0 ]; then sudo cp -f /tmp/hostaliases /etc/hosts
 
-# for good measure, make sure that we drop quickly all outgoing requests
-sudo iptables -I OUTPUT -p tcp -d 127.127.127.127 -j REJECT
+# for good measure, make sure that we drop *quickly* all outgoing requests
+# (alternative: set up a web server on ec2-54-148-20-76.us-west-2.compute.amazonaws.com)
+sudo iptables -I OUTPUT -p tcp -d $BLACKHOLEIP -j REJECT
 
 #cat /etc/hosts
 #sleep 30

faedb09ef9c8f10cf1934a64fc5ac82394bd4499
Gaetano Giunta <ggiunta@kaliop.com>
2015-06-12 10:50:36 +0200

    test

--- prerun/blacklistdomains.sh
+++ prerun/blacklistdomains.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# loop over all input arguments (a list of domain names) and blacklist them via pointing them to localhost
+# loop over all input arguments (a list of domain names) and blacklist them via pointing them to a blackhole
 
 # retrieve public IP of a VM owned by kaliop which we never use to host sites
 # this can take a couple of seconds, but we try not to blacklist any IP which MIGHT get used...
@@ -13,13 +13,14 @@ do
 done
 cat /etc/hosts >> /tmp/hostaliases
 
-cp -f /tmp/hostaliases /etc/hosts 2>/dev/null
-if [ $? -ne 0 ]; then sudo cp -f /tmp/hostaliases /etc/hosts
+sudo cp -f /tmp/hostaliases /etc/hosts
 
-# for good measure, make sure that we drop *quickly* all outgoing requests
+# for good measure, make sure that we drop *quickly* all requests to the blackholed IP
 # (alternative: set up a web server on ec2-54-148-20-76.us-west-2.compute.amazonaws.com)
+# NB: it seems that this is not installed in the Saucelabs VMs...
 sudo iptables -I OUTPUT -p tcp -d $BLACKHOLEIP -j REJECT
 
+xterm -e "cat /etc/hosts && sleep 30"
 #cat /etc/hosts
 #sleep 30
 

cb9a8fc0f2c8b3a770816c7ffad45f40fed95a69
Gaetano Giunta <ggiunta@kaliop.com>
2015-07-07 16:19:23 +0100

    again

--- prerun/blacklistdomains.sh
+++ prerun/blacklistdomains.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # loop over all input arguments (a list of domain names) and blacklist them via pointing them to a blackhole
 
-echo 'aaaa';
-
-echo 'bbbb' 1>&2;
-
 # retrieve public IP of a VM owned by kaliop which we never use to host sites
 # this can take a couple of seconds, but we try not to blacklist any IP which MIGHT get used...
 # (using localhost IPs does not seem to work)
@@ -19,19 +15,21 @@ do
 done
 cat /etc/hosts >> /tmp/hostaliases
 
-sudo cp -f /tmp/hostaliases /etc/hosts
+cat /tmp/hostaliases
+
+sudo cp -f /tmp/hostaliases /etc/hosts 2>&1
 
 # for good measure, make sure that we drop *quickly* all requests to the blackholed IP
 # (alternative: set up a web server on ec2-54-148-20-76.us-west-2.compute.amazonaws.com)
 # NB: it seems that iptables is not installed in the Saucelabs VMs...
-sudo iptables -I OUTPUT -p tcp -d $BLACKHOLEIP -j REJECT
+sudo iptables -I OUTPUT -p tcp -d $BLACKHOLEIP -j REJECT 2>&1
 
 # tests: how can we send any output to something we can grab to debug? e.g. a saucelabs log or in the video out?
 #echo 'hahaha' >> /home/chef/log/automator.log
 #sleep 30
 #xterm -e "cat /etc/hosts && sleep 60"
 
-cat /etc/hosts
+#cat /etc/hosts
 #sleep 30
 
 # if running via sauceconnect - we might possibly do this instead...
